02d2dcb23d287200c58c3e09fbaaf32a2184698c
igorthedci <foumart@ukr.net>
2019-09-26 22:50:28 +0300

    home work 05.03 Dog

--- src/main/java/task_03_dog/Dog.java
+++ src/main/java/task_03_dog/Dog.java
@@ -10,20 +10,33 @@ public class Dog {
     private String name;
     private int age;
 
+    private String defaultName = "Dog";
+    private int defaultAge = 1;
+
+    // CONSTRUCTORS
+    public Dog() {
+        this.name = defaultName;
+        this.age = defaultAge;
+    }
+    public Dog(int age) {
+        this.name = defaultName;
+        this.age = (age > 0) ? age : defaultAge;
+    }
+    public Dog(String name) {
+        this.name = name.isEmpty() ? defaultName : name;
+        this.age = defaultAge;
+    }
     public Dog(int age, String name) {
-
-        this.age = age;
-        this.name = name;
+        this.age = (age > 0) ? age : defaultAge;
+        this.name = name.isEmpty() ? defaultName : name;
     }
 
+    // AGE - getter, setter
     public int getAge() { return this.age; }
-    public void setAge(int age) { this.age = age; }
+    public void setAge(int age) { this.age = (age > 0) ? age : defaultAge; }
 
+    // NAME - getter, setter
     public String getName() { return this.name; }
-    public void setName(String name) { this.name = name; }
+    public void setName(String name) { this.name = name.isEmpty() ? defaultName : name; }
 
-
-    public static void main(String[] args) {
-
-    }
 }
